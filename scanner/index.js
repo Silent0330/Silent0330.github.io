4b5ed8cd5d6cc4961e0083e78e846a21b30e4d4a
unknown <joe25142021@gmail.com>
2022-09-25 22:09:18 +0800

    init

--- scanner/index.js
+++ scanner/index.js
@@ -1,7 +1,5 @@
 var cam_off = true;
-var key = '63770c2b81de6c49c98228baccbfcac0dd732d9663a2bab891070ed924a43d40';
 const input_cam_switch = document.getElementById("input_cam_switch");
-const text_key = document.getElementById("text_key");
 const select_onsite = document.getElementById("select_onsite");
 const html5QrCode = new Html5Qrcode("reader");
 const config = {
@@ -11,18 +9,22 @@ const config = {
         height: 640
     }
 };
-const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-    stop_scan();
-    console.log(decodedText);// 這裡已經可以正確彈出掃描結果，但點按無法跳轉
-    let hash = CryptoJS.HmacSHA256(decodedText, key);
-    hash = hash.toString(CryptoJS.enc.Hex);
+
+let initial = false; 
+var line_uid = '';
+var key = '';
+
+liff.init({
+    'liffId':  '1657500811-r2lOxyne',
+}).then(function(response) {
+    // 這邊開始寫使用其他功能
+    console.log(response);
+    line_uid = response.context.userId;
     let data = {
-        'action': '主日簽到',
-        'line_uid': decodedText,
-        'onsite_online': select_onsite.options[select_onsite.selectedIndex].value,
-        'valid_code': hash,
+        'action': 'get_key',
+        'line_uid': line_uid
     };
-    fetch('https://script.google.com/macros/s/AKfycbwYftlyJsjO0Pkn0hGh9RNDxRrogh4yPXWuJZT5d65rFklXeyhLuqE-zgwNdJ3DGg62/exec', 
+    fetch('https://script.google.com/macros/s/AKfycbyhphd70WeEC5jiogYx-BtihI6fPnHpuRhXXjRp21y-qpe3ea8-ul5i6uvNjl33JQpF/exec', 
         {
             body: JSON.stringify(data),
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -31,17 +33,60 @@ const qrCodeSuccessCallback = (decodedText, decodedResult) => {
                 'user-agent': 'scanner',
                 'content-type': 'application/json'
             },
-            mode: 'no-cors', // no-cors, cors, *same-origin
+            mode: 'same-origin', // no-cors, cors, *same-origin
             redirect: 'follow', // manual, *follow, error
-            referrer: 'no-referrer', // *client, no-referrer
+            referrer: 'client', // *client, no-referrer
         }
     ).then((response) => {
         console.log(response);
+        initial = true;
     }).then(result => {
         console.log(result);
     }).catch((err) => {
         console.log('錯誤:', err);
     });
+    
+}).catch(function(error) {
+    console.log(`init err: ${err}`);
+    alert(`init err: ${err}`);
+});
+
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    if (initial) {
+        stop_scan();
+        console.log(decodedText);// 這裡已經可以正確彈出掃描結果，但點按無法跳轉
+        let hash = CryptoJS.HmacSHA256(decodedText, key);
+        hash = hash.toString(CryptoJS.enc.Hex);
+        let data = {
+            'action': '主日簽到',
+            'line_uid': decodedText,
+            'onsite_online': select_onsite.options[select_onsite.selectedIndex].value,
+            'valid_code': hash,
+        };
+        fetch('https://script.google.com/macros/s/AKfycbyhphd70WeEC5jiogYx-BtihI6fPnHpuRhXXjRp21y-qpe3ea8-ul5i6uvNjl33JQpF/exec', 
+            {
+                body: JSON.stringify(data),
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                credentials: 'omit', // include, same-origin, *omit
+                headers: {
+                    'user-agent': 'scanner',
+                    'content-type': 'application/json'
+                },
+                mode: 'same-origin', // no-cors, cors, *same-origin
+                redirect: 'follow', // manual, *follow, error
+                referrer: 'client', // *client, no-referrer
+            }
+        ).then((response) => {
+            console.log(response);
+        }).then(result => {
+            console.log(result);
+        }).catch((err) => {
+            console.log('錯誤:', err);
+        });
+    }
+    else {
+      alert('初始失敗');
+    }
 };
 
 
@@ -87,44 +132,23 @@ function onclick_btn_key() {
     text_key.value = "";
 }
 
-text_key.addEventListener("keydown", onkeydown_text_key);
+function onclick_page_key_scanner() {
+  let data = {
+      'action': '主動掃描',
+      'line_uid': line_uid,
+  };
+  let form = document.createElement('form');
+  form.method = 'post';
+  form.action = '/';
+  document.body.appendChild(form);
 
-function onkeydown_text_key(e) {
-    if (e.keyCode == 13) {
-        key = text_key.value;
-        text_key.value = "";
-    }
-}
+  for (const key in data) {
+      const formField = document.createElement('input');
+      formField.type = 'hidden';
+      formField.name = key;
+      formField.value = data[key];
 
-function onclick_btn_test() {
-    console.log(select_onsite.options[select_onsite.selectedIndex].value);
-    let line_uid = "U7d97232f259c299dca82a5fd761debc3";
-    let hash = CryptoJS.HmacSHA256(line_uid, key);
-    hash = hash.toString(CryptoJS.enc.Hex);
-    let data = {
-        'action': '主日簽到',
-        'line_uid': line_uid,
-        'onsite_online': select_onsite.options[select_onsite.selectedIndex].value,
-        'valid_code': hash,
-    };
-    fetch('https://script.google.com/macros/s/AKfycbwwjcUC9Z9Vya_myJ1kRNbDANiwdaaDmDgOSwVaMO7icD6EXtd24GtRSac3GvwSmLNB/exec', 
-        {
-            body: JSON.stringify(data),
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            credentials: 'omit', // include, same-origin, *omit
-            headers: {
-                'user-agent': 'scanner',
-                'content-type': 'application/json'
-            },
-            mode: 'no-cors', // no-cors, cors, *same-origin
-            redirect: 'follow', // manual, *follow, error
-            referrer: 'no-referrer', // *client, no-referrer
-        }
-    ).then((response) => {
-        console.log(response);
-    }).then(result => {
-        console.log(result);
-    }).catch((err) => {
-        console.log('錯誤:', err);
-    });
+      form.appendChild(formField);
+  }
+  form.submit();
 }
