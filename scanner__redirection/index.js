bca6612e9a81480ff3eff5e98412fb5a526ff4f0
unknown <joe25142021@gmail.com>
2022-09-26 08:54:37 +0800

    change to redirection

---
+++ scanner__redirection/index.js
@@ -0,0 +1,37 @@
+liff.init({
+    'liffId':  '1657500811-r2lOxyne',
+}).then(function() {
+    // 這邊開始寫使用其他功能
+    liff.getProfile()
+    .then(profile => {
+        let line_uid = profile.userId;
+        let displayName = profile.displayName;
+        let pictureUrl = profile.pictureUrl;
+        let data = {
+            'action': '自助報到',
+            'line_uid': line_uid,
+            'displayName': displayName,
+            'pictureUrl': pictureUrl
+        };
+        let form = document.createElement('form');
+        form.method = 'post';
+        form.action = 'https://script.google.com/macros/s/AKfycbyOvOJPto2rASg69y6UHRB05KCGepO9-BLKIIhe7r6tVAxLGdH9gV7Xnm8h3vPupLRu/exec';
+        document.body.appendChild(form);
+
+        for (const key in data) {
+            const formField = document.createElement('input');
+            formField.type = 'hidden';
+            formField.name = key;
+            formField.value = data[key];
+
+            form.appendChild(formField);
+        }
+        form.submit();
+    })
+    .catch((err) => {
+        alert(`profile err: ${err}`);
+    });
+    
+}).catch(function(err) {
+    alert(`init err: ${err}`);
+});
