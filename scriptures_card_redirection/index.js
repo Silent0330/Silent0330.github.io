55d04f6a612e0451e8062ea1d98b48b5c0205b67
unknown <joe25142021@gmail.com>
2022-12-20 23:52:11 +0800

    新增liff

--- scriptures_card_redirection/index.js
+++ scriptures_card_redirection/index.js
@@ -62,46 +62,46 @@ function animation() {
     frame = (frame+1)%4;
 }
 
-// liff.init({
-//     'liffId':  '1657754998-0PAZxgke',
-// }).then(function() {
-//     // 這邊開始寫使用其他功能
-//     const context = liff.getContext();
-//     console.log(context);
-//     if (!liff.isLoggedIn()){
-//         liff.login();
-//     }
-//     else {
-//         liff.getProfile()
-//         .then(profile => {
-//             let line_uid = profile.userId;
-//             let displayName = profile.displayName;
-//             let data = {
-//                 'action': '取得2023經文卡',
-//                 'line_uid': line_uid,
-//                 'line_displayName': displayName
-//             };
-//             console.log(data);
-//             let form = document.createElement('form');
-//             form.method = 'post';
-//             form.action = 'https://script.google.com/macros/s/AKfycbx87Gml8O2RK6_-YobYBVqTwFrDe_dJCojp46ZONh4I7UQ5NZ1e-mECVZRSdCh32KpwIw/exec';
-//             document.body.appendChild(form);
+liff.init({
+    'liffId':  '1657754998-0PAZxgke',
+}).then(function() {
+    // 這邊開始寫使用其他功能
+    const context = liff.getContext();
+    console.log(context);
+    if (!liff.isLoggedIn()){
+        liff.login();
+    }
+    else {
+        liff.getProfile()
+        .then(profile => {
+            let line_uid = profile.userId;
+            let displayName = profile.displayName;
+            let data = {
+                'action': '取得2023經文卡',
+                'line_uid': line_uid,
+                'line_displayName': displayName
+            };
+            console.log(data);
+            let form = document.createElement('form');
+            form.method = 'post';
+            form.action = 'https://script.google.com/macros/s/AKfycbx87Gml8O2RK6_-YobYBVqTwFrDe_dJCojp46ZONh4I7UQ5NZ1e-mECVZRSdCh32KpwIw/exec';
+            document.body.appendChild(form);
     
-//             for (const key in data) {
-//                 const formField = document.createElement('input');
-//                 formField.type = 'hidden';
-//                 formField.name = key;
-//                 formField.value = data[key];
+            for (const key in data) {
+                const formField = document.createElement('input');
+                formField.type = 'hidden';
+                formField.name = key;
+                formField.value = data[key];
     
-//                 form.appendChild(formField);
-//             }
-//             form.submit();
-//         })
-//         .catch((err) => {
-//             alert(`profile err: ${err}`);
-//         });
-//     }
+                form.appendChild(formField);
+            }
+            form.submit();
+        })
+        .catch((err) => {
+            alert(`profile err: ${err}`);
+        });
+    }
     
-// }).catch(function(err) {
-//     alert(`init err: ${err}`);
-// });
+}).catch(function(err) {
+    alert(`init err: ${err}`);
+});
